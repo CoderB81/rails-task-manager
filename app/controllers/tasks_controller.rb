3b00b0d70f3490130ca973137cf9990a13cb0d5c
Brian Burrow <bburrow81@gmail.com>
2020-08-16 11:23:43 +0200

    Task MGR partial

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,39 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(private_method)
+    @task.save
+  end
+
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def some_private_method
+    # use strong params
+    # see lecture slides
+  end
+
 end
+
+
